a194dd6e9e7517c5ec6f5eed52a64b3ce8bdde88
ClaireGenre <cgenre@hotmail.fr>
2018-04-29 17:09:06 -0700

    fixed style things

--- takai/forms.py
+++ takai/forms.py
@@ -72,6 +72,10 @@ class ApplicationForm(ModelForm):
     class Meta:
         model = Application
         fields = ['semester', 'year', 'school', 'major', 'qualities', 'num_hours_week']
+        labels = {
+            'qualities': ('What qualities do you have that make you a good student mentor?'),
+            'num_hours_week': ('Maximum number of hours available per week:'),
+        }
 
 class AvailabilityForm(ModelForm):
     AVAILABILITY_CHOICES  = (
